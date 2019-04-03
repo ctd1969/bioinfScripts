8beaf228a95d50468af74dd516d5a554939e3a58
evoclive <ctdarwell@gmail.com>
2019-04-03 17:10:14 +0700

    Update stacksFastaWriter.py

--- stacksFastaWriter.py
+++ stacksFastaWriter.py
@@ -9,81 +9,59 @@ def main():
     except:
         print('\n*** FOLDER "'+sys.argv[3]+'" ALREADY EXISTS!!! ***')
         return
-    locs=[] #locus names
-    haps=[] #grouped loci
-    seqs=[]
-    cnts=[]
-    cnt=0
-    prev='>CLocus_1' #dummy variable to allow first id of when we move to next locus
+    locs, haps, seqs, cnts, cnt = [], [], [], [], 0 #locus names, grouped loci
+    prev = '>CLocus_1' #dummy variable to allow first id of when we move to next locus
     for line in open(sys.argv[2]): #go thru fasta file, store data and list of all loci
         if line.startswith('>'):
-            locs.append(line)
-            haps.append(line.split('_')[0]+'_'+line.split('_')[1]) #list all loci
-            cnts.append(cnt)
-            cnt+=1
+            locs.append(line), haps.append(line.split('_')[0]+'_'+line.split('_')[1]), cnts.append(cnt)
+            cnt += 1
         else:
             seqs.append(line)
 
-    cnt=0
-    taxCnt=0
-    subseqs=[]
-    sublocs=[]
-    tmpTaxa=[]
+    cnt, taxCnt, subseqs, sublocs, tmpTaxa = 0, 0, [], [], []
+
     #process all loci & id which taxa are missing
     for hap in haps:
-        if locs[cnt].split(' ')[1][1:-2] not in tmpTaxa and hap==prev:
-            tmpTaxa.append(locs[cnt].split(' ')[1][1:-2])
-            subseqs.append(seqs[cnt])
-            sublocs.append(locs[cnt])
-            taxCnt+=1
-        if hap!=prev and taxCnt> int(len(taxa)/2) - 1: #min 50% taxa per locus
-            print(taxCnt)
-            snps=[]
+        if locs[cnt].split(' ')[1][1:-2] not in tmpTaxa and hap == prev:
+            tmpTaxa.append(locs[cnt].split(' ')[1][1:-2]), subseqs.append(seqs[cnt]), sublocs.append(locs[cnt])
+            taxCnt += 1
+        if hap != prev and taxCnt > int(len(taxa)/2) - 1: #min 50% taxa per locus
+            snps = []
             for subseq in subseqs:
-                cnt2=0
+                cnt2 = 0
                 for bp in subseq[:-1]:
-                    if bp!=subseqs[0][cnt2]:
+                    if bp != subseqs[0][cnt2]:
                         snps.append(cnt2)
-                    cnt2+=1
-                    cnt3=len(subseq[:-1])
-            snpset=set(snps)
-            if len(snpset)>0: #Min. no. SNPS per locus
-                g=open(sys.argv[3]+'/'+prev[1:]+'.fas','w')
+                    cnt2 += 1
+                    cnt3 = len(subseq[:-1])
+            snpset = set(snps)
+            if len(snpset) > 0: #Min. no. SNPS per locus
+                g = open(sys.argv[3] + '/'+prev[1:] + '.fas', 'w')
                 for tax in taxa:
                     if tax not in tmpTaxa:
-                        g.write('>'+tax+'\n')
-                        g.write('-'*cnt3)
-                        g.write('\n')
+                        g.write('>'+tax+'\n' + '-' * cnt3 + '\n')
                     else:
-                        g.write('>'+tax+'\n'+subseqs[tmpTaxa.index(tax)])
+                        g.write('>' + tax + '\n' + subseqs[tmpTaxa.index(tax)])
                 g.close()
             #reset variables
-            tmpTaxa=[]
-            subseqs=[]
-            sublocs=[]
-            tmpTaxa.append(locs[cnt].split(' ')[1][1:-2])
-            subseqs.append(seqs[cnt])
-            sublocs.append(locs[cnt])
-            taxCnt=0
-        if hap!=prev and taxCnt < int(len(taxa)/2): #reset variables for loci with too few taxa
-            tmpTaxa=[]
-            subseqs=[]
-            sublocs=[]
-            tmpTaxa.append(locs[cnt].split(' ')[1][1:-2])
-            subseqs.append(seqs[cnt])
-            sublocs.append(locs[cnt])
-            taxCnt=0
-        cnt+=1
-        prev=hap
+            tmpTaxa, subseqs, sublocs = [], [], []
+            tmpTaxa.append(locs[cnt].split(' ')[1][1:-2]), subseqs.append(seqs[cnt]), sublocs.append(locs[cnt])
+            taxCnt = 0
+        if hap != prev and taxCnt < int(len(taxa)/2): #reset variables for loci with too few taxa
+            tmpTaxa, subseqs, sublocs = [], [], []
+            tmpTaxa.append(locs[cnt].split(' ')[1][1:-2]), subseqs.append(seqs[cnt]), sublocs.append(locs[cnt])
+            taxCnt = 0
+        cnt += 1
+        prev = hap
 
 #extract taxon list from VCF file
 def taxList(vcfFile):
-    taxa=[]
+    taxa = []
     for line in open(vcfFile):
-        z=line.strip().split('\t')
+        z = line.strip().split('\t')
         if line.startswith('#CHROM'):
             for tax in z:
-                if z.index(tax)>8: # taxa listed after 8th column
+                if z.index(tax) > 8: # taxa listed after 8th column
                     taxa.append(tax.strip()) #list taxon name
             break
     return taxa
